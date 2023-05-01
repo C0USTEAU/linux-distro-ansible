3acbefbf707c91129148b0cdb3bec043a129c7e4
Feargal Egan <egan.feargal.web@gmail.com>
2023-05-01 17:09:01 +0000

    Some fixes for generic Arch setup - Adding yay using makepgk in bootstrap - Additional packages added for display manager and greeter - Still need to add auto enabling of lightdm and greeter

--- bootstrap/pacman-bootstrap.sh
+++ bootstrap/pacman-bootstrap.sh
@@ -1,5 +1,20 @@
 sudo pacman -S archlinux-keyring
 sudo pacman -Syyuu
 sudo pacman -S ansible 
+sudo pacman -S git
 ansible --version
 sudo cp hosts /etc/ansible/hosts
+
+cd /tmp
+sudo git clone https://aur.archlinux.org/yay-git.git 
+sudo chown -R feargal:feargal yay-git
+cd -
+cd /tmp/yay-git
+makepkg -si
+cd -
+sudo rm -rf /tmp/yay-git
+
+git clone "https://github.com/mnussbaum/ansible-yay.git" /tmp/ansible-yay
+sudo mkdir -p /usr/share/ansible/plugins/modules
+sudo cp /tmp/ansible-yay/yay /usr/share/ansible/plugins/modules/
+sudo rm -rf /tmp/ansible-yay
